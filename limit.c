e682115c0e923da84aa5c9d1b3d899315b5e5a5f
Erik Nordstroem <contact@erikano.net>
2014-10-24 22:42:58 +0200

    Rewrite some dialogue and try better to ensure that buffer is flushed to screen where it needs to (by printing newlines in new calls). Sleep for a few seconds before revealing what happens. Move intro into a function of its own.

--- limit.c
+++ limit.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <SDL2/SDL.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 typedef struct player_ {
   int health;
 } player;
 
-int level1 (player* player1, player* player2)
+typedef struct stats_level_ {
+  int error;
+} stats_level;
+
+// "L" -- text mode
+int level1 (SDL_Window* window, SDL_Surface* screenSurface, stats_level* stats_level1, player* player1, player* player2)
 {
   printf("\n  You are standing in a room.");
   printf("\n\n  A sly, female voice speaks in front of you:");
-  printf("\n\n    The LIMIT of what you can do is:");
+  printf("\n\n    The limit of what you can do is:");
   printf("\n\n      [l]ook.");
   printf("\n\n  What do you do?");
-  printf("\n\n      : ");
+  printf("\n\n    : ");
   char c = getchar();
   if (c == 'l')
   {
@@ -21,100 +30,65 @@ int level1 (player* player1, player* player2)
     printf("\n\n      Your eardrums shatter.");
     printf("\n\n        You fall to the ground.");
     printf("\n\n          You hit your head on a sharp object.");
-    printf("\n\n            The last thing you hear is a sobbing voice:");
+    printf("\n\n            Still from your left, you hear a sobbing voice:");
     printf("\n\n              I didn't mean to...");
+    printf("\n");
     (*player1).health = 0;
-    printf("\n\n            You die.");
+    sleep(15);
+    printf("\n            You die.");
+    printf("\n");
+    (*stats_level1).error = EXIT_SUCCESS;
     return false;
   }
   else if (c == 'L')
   {
-    printf("\n  Not listening to the obviously malicious voice, you leave this room.");
-    printf("\n\n    Outside is nothing.");
-    printf("\n\n      You *L*eave this world.");
-    return false;
+    printf("\n  You ignore the obviously malicious voice.");
+    printf("\n\n    You leave this room.");
+    printf("\n\n      Outside is nothing.");
+    printf("\n\n        You *L*eave this world.");
+    printf("\n");
+    sleep(15);
+    printf("\n          alive.");
+    printf("\n");
+    (*stats_level1).error = EXIT_SUCCESS;
+    return true;
   }
   else
   {
-    printf("\n  The voice now whips you:");
+    printf("\n  The voice now whips your ears:");
     printf("\n\n    MIND YOUR INPUT!");
+    printf("\n");
+    sleep(15);
     (*player1).health = 0;
-    printf("\n\n  Your life force is sucked out.");
+    printf("\n      Your life force is sucked out.");
+    printf("\n\n        You die.");
+    printf("\n");
+    (*stats_level1).error = EXIT_SUCCESS;
     return false;
   }
 }
 
-#include <SDL2/SDL.h>
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-// ...
-
-/* After "L-I-M-I-T" has been collected, the good one speaks:
- *
- *   You were able to see through the lies of the evil.
- *
- * One after another, she gently picks up the letters.
- *
- * She floats them in the air and forms them to the word.
- *
- * She looks at you.
- *
- * With a wave of her hand, the letters disappear.
- *
- * Fade to black
- *
- * (Play a deep note)
- *
- * "THE END"
- */
-
-int main (int argc, char* argv[])
+int intro (SDL_Window* window, SDL_Surface* screenSurface, stats_level* stats_intro, player* player1, player* player2)
 {
-  player player1;
-  player1.health = 100;
-  player player2;
-  player2.health = 100;
-
-  bool made_it_through = true;
-  while (made_it_through && player1.health > 0 && player2.health)
-  {
-    made_it_through = level1(&player1, &player2);
-  }
-
-  if (player1.health <= 0)
-  {
-    printf("\n\n  Dead people don't play games.");
-    printf("\n\n    GAME OVER.");
-  }
-  else
-  {
-    printf("\n\n  You survived.");
-    printf("\n\n    Mere survival is not sufficient.");
-    printf("\n\n      GAME OVER.");
-  }
-  printf("\n\n");
-
-  /*
-  SDL_Window* window = NULL;
-  SDL_Surface* screenSurface = NULL;
-
   if (SDL_Init(SDL_INIT_VIDEO) < 0 )
   {
     fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    exit(EXIT_FAILURE);
+    (*player1).health = 0;
+    (*stats_intro).error = EXIT_FAILURE;
+    return false;
   }
 
   window = SDL_CreateWindow("LIMIT -- Sonen Game Jam 2014, Team Erik", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
   if (window == NULL)
   {
     fprintf(stderr, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
-    exit(EXIT_FAILURE);
+    (*player1).health = 0;
+    (*stats_intro).error = EXIT_FAILURE;
+    return false;
   }
 
   screenSurface = SDL_GetWindowSurface(window);
 
-  // Intro
   SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
   SDL_UpdateWindowSurface(window);
   SDL_Delay(125);
@@ -126,33 +100,96 @@ int main (int argc, char* argv[])
     SDL_Delay(5);
   }
 
-  // Game loop
-  bool gameover = false;
-  bool quit = false;
-  SDL_Event e;
-  while (!(gameover || quit))
+  (*stats_intro).error = EXIT_SUCCESS;
+  return true;
+}
+
+// "I" -- black and white
+
+// "M" - greyscale and sound
+
+// "I" - colors
+
+// "T" - 3D
+
+/* After "L-I-M-I-T" has been collected, the good one appears.
+ *
+ * One after another, she gently picks up the letters.
+ *
+ * She floats them in the air and forms them to the word.
+ *
+ * She looks at you.
+ *
+ * With a wave of her hand, the letters disappear.
+ *
+ * Fade to black
+ *
+ * (Play a *deep* note)
+ *
+ * "THE END"
+ */
+
+int main (int argc, char* argv[])
+{
+  stats_level stats_level_current;
+  player player1;
+  player1.health = 100;
+
+  SDL_Window* window = NULL;
+  SDL_Surface* screenSurface = NULL;
+
+  player player2;
+  player2.health = 100;
+
+  bool made_it_through = level1(window, screenSurface, &stats_level_current, &player1, &player2);
+  if (made_it_through && player1.health > 0 && player2.health > 0)
   {
-    // Event loop
-    while(SDL_PollEvent(&e) != 0)
+    made_it_through = intro(window, screenSurface, &stats_level_current, &player1, &player2);
+  }
+  if (made_it_through && player1.health > 0 && player2.health > 0)
+  {
+    // Game loop
+    bool gameover = false;
+    bool quit = false;
+    SDL_Event e;
+    while (!(gameover || quit))
     {
-      if(e.type == SDL_QUIT)
+      // Event loop
+      while(SDL_PollEvent(&e) != 0)
       {
-        quit = true;
+        if(e.type == SDL_QUIT)
+        {
+          fprintf(stderr, "\n  User abort.");
+          fprintf(stderr, "\n\n    GAME OVER.\n");
+          stats_level_current.error = EXIT_FAILURE;
+          quit = true;
+        }
       }
+      SDL_Delay(5);
     }
-    SDL_Delay(5);
+  }
+  if (stats_level_current.error != EXIT_FAILURE)
+  {
+    if (player1.health <= 0)
+    {
+      printf("\n  Dead people don't play games.");
+      printf("\n\n    GAME OVER.");
+    }
+    else
+    {
+      printf("\n  You survived.");
+      printf("\n\n    Mere survival is not sufficient.");
+      printf("\n\n      GAME OVER.");
+    }
+    printf("\n\n");
   }
 
   SDL_DestroyWindow(window);
   SDL_Quit();
 
-  if (gameover)
+  if (stats_level_current.error == EXIT_FAILURE)
   {
-    fprintf(stderr, "Game over.\n");
-    return EXIT_SUCCESS;
+    return EXIT_FAILURE;
   }
-
-  fprintf(stderr, "User abort.\n");
-  return EXIT_FAILURE;
-  */
+  return EXIT_SUCCESS;
 }
